21987e154cf29e163b74046d5fadbd21d418d44b
usuario01 <nicolasgonzalezmoller@gmail.com>
2024-08-17 14:04:35 -0400

    fetch

--- src/app/tasks/new/page.tsx
+++ src/app/tasks/new/page.tsx
@@ -1,13 +1,32 @@
-import { useState } from "react"
+'use client'
+import { ChangeEvent, FormEvent, useState } from "react"
 
 function FormPage() {
+
+  const [newTask, setNewTask] =useState({title:"",
+    description:""
+  })
+
+  const handleSubmit = (e: FormEvent) => {
+      e.preventDefault()
+      console.log(newTask)
+      fetch('/api/tasks',{method:"POST",body:JSON.stringify(newTask)})
+  }
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement|HTMLTextAreaElement>) =>{
+      // console.log(e.target.value)
+      setNewTask({...newTask, [e.target.name]:e.target.value})
+  } 
   return (
     <div className='h-[calc(100vh-7rem)] flex justify-center items-center'>
-      <form action="">
+      <form action="" onSubmit={handleSubmit}>
         <input type="text" name='title' placeholder='Titulo'
-         className='bg-gray-800 border-2 w-full p-4 rounded-lg my-4'/>
+         className='bg-gray-800 border-2 w-full p-4 rounded-lg my-4' 
+         onChange={handleChange}/>
+        
          <textarea rows={3}
-         name='Description' placeholder='tarea' className='bg-gray-800 border-2 w-full p-4 rounded-lg my-4'></textarea>
+         name='Description' placeholder='tarea' className='bg-gray-800 border-2 w-full p-4 rounded-lg my-4'
+         onChange={handleChange}></textarea>
          <button className='bg-green-600 text-white hover:bg-green-800 font-bold py-2 px-4 rounded-lg'>
             save
          </button>
